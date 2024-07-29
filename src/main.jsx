e126b11d5074442dc20aae7ab85c4b410cafa77c
Bodi_Ali <bodiali59@gmail.com>
2024-07-29 17:04:48 +0300

    Nest routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
-import ErrorPage from "./pages/ErrorPage";
+import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
 import "./reset.css";
+import HomePage, { loader as homePageLoader } from "./pages/HomePage/HomePage.jsx";
 
+//Todo: implement image carousel from data
 //TODO: Style ErrorPage
 
 const router = createBrowserRouter([
@@ -12,6 +14,14 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+        loader: homePageLoader,
+        errorElement: <ErrorPage />,
+      },
+    ],
   },
 ]);
 
