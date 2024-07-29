bd193942e6ed3602b10850694a45ecd3bb929305
Bodi_Ali <bodiali59@gmail.com>
2024-07-29 07:52:49 +0300

    Add navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,16 @@
+import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
+
 function Header() {
   return (
     <>
-      <header>
-        <h1></h1>
+      <header className={styles.header}>
+        <h1>GameVault</h1>
+        <div className={styles.navContainer}>
+          <NavLink to="">Home</NavLink>
+          <NavLink to="shop">Shop</NavLink>
+          <NavLink to="about">About</NavLink>
+        </div>
       </header>
     </>
   );
