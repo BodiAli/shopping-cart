9a4b3d94ba51c11a90653c62f26a4eb9a8994522
Bodi_Ali <bodiali59@gmail.com>
2024-07-29 10:27:13 +0300

    Update tests to use react-router

--- src/tests/Header.test.jsx
+++ src/tests/Header.test.jsx
@@ -1,20 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import Header from "../components/Header/Header";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import routes from "../router/routes";
 import { expect } from "vitest";
+
+const router = createMemoryRouter(routes);
+const wrapper = () => <RouterProvider router={router}></RouterProvider>;
 
 describe("Header component", () => {
   test("should render correct elements", () => {
-    const { container } = render(<Header />);
+    const { container } = render(<Header />, { wrapper: wrapper });
     expect(container).toMatchSnapshot();
   });
 
   test("should render header element", () => {
-    render(<Header />);
+    render(<Header />, { wrapper: wrapper });
     expect(screen.getByRole("heading")).toBeInTheDocument();
   });
 
   test("should render home button", () => {
-    render(<Header />);
+    render(<Header />, { wrapper: wrapper });
     expect(screen.getByRole("link", { name: "Home" })).toBeInTheDocument();
   });
 });
