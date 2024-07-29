8b83fa04a7e1f282737ab839d4a197056dd92115
Bodi_Ali <bodiali59@gmail.com>
2024-07-29 17:04:21 +0300

    Use Outlet for nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header";
 import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
+      <Outlet />
     </>
   );
 }
