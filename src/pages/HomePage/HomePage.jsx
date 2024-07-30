3ac4e5e34db0737ef446d7258dd92acb929adbed
Bodi_Ali <bodiali59@gmail.com>
2024-07-30 06:57:52 +0300

    Create card structure

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,9 +2,11 @@ import styles from "./HomePage.module.css";
 import getData from "../../utils/fetchData";
 import { useLoaderData } from "react-router-dom";
 
-function HomePage() {
-  // const data = useLoaderData();
+//TODO: return jsx from description
 
+function HomePage() {
+  const data = useLoaderData();
+  console.log(data);
   return (
     <main className={styles.main}>
       <div className={styles.descriptionContainer}>
@@ -15,7 +17,19 @@ function HomePage() {
         </p>
       </div>
       <div className={styles.carousel}>
-        <div className={styles.card}></div>
+        <button>Back</button>
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <div
+              className={styles.backGround}
+              style={{ backgroundImage: `url(${data[0].sample_cover.thumbnail_image})` }}
+            ></div>
+            <img src={data[0].sample_cover.thumbnail_image} alt="" />
+          </div>
+          <h3>{data[0].title}</h3>
+          {data[0].description}
+        </div>
+        <button>Forward</button>
         <div className={styles.pagination}>
           <div className={styles.circle}></div>
           <div className={styles.circle}></div>
@@ -28,8 +42,11 @@ function HomePage() {
 }
 
 async function loader() {
-  const data = await getData("/random?limitt=5&");
-  return data;
+  const data = await getData("/random?limit=5&format=normal&");
+  if (!data) {
+    throw new Error("Unable to fetch data");
+  }
+  return data.games;
 }
 
 export { loader };
