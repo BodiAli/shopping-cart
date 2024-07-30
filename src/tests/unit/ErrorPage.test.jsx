7b08f4a2df92ecbe5dab91671e06e165653d74fd
Bodi_Ali <bodiali59@gmail.com>
2024-07-30 17:38:10 +0300

    Create test file for ErrorPage

---
+++ src/tests/unit/ErrorPage.test.jsx
@@ -0,0 +1,7 @@
+// import { render, screen } from "@testing-library/react";
+// import ErrorPage from "../../pages/ErrorPage/ErrorPage";
+// import { createMemoryRouter, RouterProvider } from "react-router-dom";
+// import routes from "../../router/routes";
+
+// const router = createMemoryRouter(routes);
+// const wrapper = () => <RouterProvider router={router}></RouterProvider>;
