58772fe186afdf205387abf095860468a7e10310
Bodi_Ali <bodiali59@gmail.com>
2024-07-30 09:02:43 +0300

    Structure description && add loader to component property to enable fast refresh

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,23 +1,30 @@
-import styles from "./HomePage.module.css";
-import getData from "../../utils/fetchData";
 import { useLoaderData } from "react-router-dom";
-
-//TODO: return jsx from description
+import getData from "../../utils/fetchData";
+import back from "../../assets/back.svg";
+import forward from "../../assets/forward.svg";
+import styles from "./HomePage.module.css";
 
 function HomePage() {
   const data = useLoaderData();
   console.log(data);
+
   return (
     <main className={styles.main}>
-      <div className={styles.descriptionContainer}>
-        <h2>Welcome to GameVault, Your Ultimate Video Game Destination</h2>
+      <div className={styles.headerContainer}>
+        <h2>
+          Welcome to GameVault
+          <br />
+          Your Ultimate Video Game Destination
+        </h2>
         <p>
           Discover a vast selection of video games across all genres and platforms. Whether you&apos;re a
           casual gamer or a hardcore enthusiast, we have everything you need to elevate your gaming experience
         </p>
       </div>
       <div className={styles.carousel}>
-        <button>Back</button>
+        <button>
+          <img src={back} alt="back" />
+        </button>
         <div className={styles.card}>
           <div className={styles.imageContainer}>
             <div
@@ -26,10 +33,14 @@ function HomePage() {
             ></div>
             <img src={data[0].sample_cover.thumbnail_image} alt="" />
           </div>
-          <h3>{data[0].title}</h3>
-          {data[0].description}
+          <div className={styles.descriptionContainer}>
+            <h3>{data[0].title}</h3>
+            <div dangerouslySetInnerHTML={{ __html: data[0].description }}></div>
+          </div>
         </div>
-        <button>Forward</button>
+        <button>
+          <img src={forward} alt="forward" />
+        </button>
         <div className={styles.pagination}>
           <div className={styles.circle}></div>
           <div className={styles.circle}></div>
@@ -49,5 +60,5 @@ async function loader() {
   return data.games;
 }
 
-export { loader };
+HomePage.loader = loader;
 export default HomePage;
