4b3fc8175d9bf0f9eb8ac30098b655e10e9a0509
Bodi_Ali <bodiali59@gmail.com>
2024-07-30 17:40:42 +0300

    Render Header component only

--- src/tests/unit/Header.test.jsx
+++ src/tests/unit/Header.test.jsx
@@ -1,21 +1,29 @@
 import { render, screen } from "@testing-library/react";
 import Header from "../../components/Header/Header";
 import { createMemoryRouter, RouterProvider } from "react-router-dom";
-import routes from "../../router/routes";
-import { expect } from "vitest";
 
-const router = createMemoryRouter(routes);
-const wrapper = () => <RouterProvider router={router}></RouterProvider>;
+const router = createMemoryRouter([
+  {
+    path: "/",
+    element: <Header />,
+  },
+]);
+const wrapper = () => <RouterProvider router={router} />;
 
 describe("Header component", () => {
-  test("should render correct elements", () => {
+  test("should render correct elements", async () => {
     const { container } = render(<Header />, { wrapper: wrapper });
     expect(container).toMatchSnapshot();
   });
 
   test("should render header element", () => {
     render(<Header />, { wrapper: wrapper });
-    expect(screen.getByRole("heading")).toBeInTheDocument();
+    expect(screen.getByRole("banner")).toBeInTheDocument();
+  });
+
+  test("should render website name header", () => {
+    render(<Header />, { wrapper: wrapper });
+    expect(screen.getByRole("heading", { name: "GameVault" })).toBeInTheDocument();
   });
 
   test("should render nav buttons", () => {
