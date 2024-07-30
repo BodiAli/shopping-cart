bc86235e88885749fdf76dae7b7230bb46b90c59
Bodi_Ali <bodiali59@gmail.com>
2024-07-30 17:38:53 +0300

    Update routes for tests

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,11 +1,24 @@
 import App from "../App";
-import ErrorPage from "../pages/ErrorPage";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import HomePage from "../pages/HomePage/HomePage";
 
 const routes = [
   {
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+        loader: HomePage.loader,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "shop",
+        element: <p>HIII</p>,
+      },
+    ],
   },
 ];
 
