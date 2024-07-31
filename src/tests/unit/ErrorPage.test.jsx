e8c843906dbfe7c5dcb35dc431c8f247850f7894
Bodi_Ali <bodiali59@gmail.com>
2024-07-31 12:41:06 +0300

    Create tests for Error page

--- src/tests/unit/ErrorPage.test.jsx
+++ src/tests/unit/ErrorPage.test.jsx
@@ -1,7 +1,50 @@
-// import { render, screen } from "@testing-library/react";
-// import ErrorPage from "../../pages/ErrorPage/ErrorPage";
-// import { createMemoryRouter, RouterProvider } from "react-router-dom";
-// import routes from "../../router/routes";
+import { render, screen } from "@testing-library/react";
+import { createMemoryRouter, RouterProvider, useRouteError } from "react-router-dom";
+import { vi } from "vitest";
+import ErrorPage from "../../pages/ErrorPage/ErrorPage";
 
-// const router = createMemoryRouter(routes);
-// const wrapper = () => <RouterProvider router={router}></RouterProvider>;
+vi.mock("react-router-dom", async (importOriginal) => {
+  const actual = await importOriginal();
+  return {
+    ...actual,
+    useRouteError: vi.fn(() => new Error("Unexpected error occurred")),
+  };
+});
+
+const router = createMemoryRouter([
+  {
+    path: "/",
+    element: <ErrorPage />,
+  },
+]);
+const wrapper = () => <RouterProvider router={router} />;
+
+describe("Error page", () => {
+  test("should render correct elements", () => {
+    const { container } = render(<ErrorPage />, { wrapper });
+    expect(container).toMatchSnapshot();
+  });
+
+  test("should render correct heading", () => {
+    render(<ErrorPage />, { wrapper });
+    expect(screen.getByRole("heading")).toHaveTextContent(/looks like you are lost/i);
+  });
+
+  test("should render error message", () => {
+    render(<ErrorPage />, { wrapper });
+    expect(screen.getByRole("paragraph")).toHaveTextContent("Unexpected error occurred");
+  });
+
+  test("should render error data if provided", () => {
+    useRouteError.mockReturnValueOnce({ data: "Some detailed error data" });
+    render(<ErrorPage />, { wrapper });
+    expect(screen.getByText("Some detailed error data")).toBeInTheDocument();
+  });
+
+  test("should render a link to head back home", () => {
+    render(<ErrorPage />, { wrapper });
+    const linkElement = screen.getByRole("link", { name: /head back/i });
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute("href", "/");
+  });
+});
