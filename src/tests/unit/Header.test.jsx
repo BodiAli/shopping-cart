2bfe567fc86e78206cea991a79e260a01b9590af
Bodi_Ali <bodiali59@gmail.com>
2024-07-31 12:41:52 +0300

    Test if links link to their respective paths

--- src/tests/unit/Header.test.jsx
+++ src/tests/unit/Header.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
-import Header from "../../components/Header/Header";
 import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import { expect } from "vitest";
+import Header from "../../components/Header/Header";
 
 const router = createMemoryRouter([
   {
@@ -12,24 +13,33 @@ const wrapper = () => <RouterProvider router={router} />;
 
 describe("Header component", () => {
   test("should render correct elements", async () => {
-    const { container } = render(<Header />, { wrapper: wrapper });
+    const { container } = render(<Header />, { wrapper });
     expect(container).toMatchSnapshot();
   });
 
   test("should render header element", () => {
-    render(<Header />, { wrapper: wrapper });
+    render(<Header />, { wrapper });
     expect(screen.getByRole("banner")).toBeInTheDocument();
   });
 
   test("should render website name header", () => {
-    render(<Header />, { wrapper: wrapper });
+    render(<Header />, { wrapper });
     expect(screen.getByRole("heading", { name: "GameVault" })).toBeInTheDocument();
   });
 
-  test("should render nav buttons", () => {
-    render(<Header />, { wrapper: wrapper });
-    expect(screen.getByRole("link", { name: /home/i })).toBeInTheDocument();
-    expect(screen.getByRole("link", { name: /shop/i })).toBeInTheDocument();
-    expect(screen.getByRole("link", { name: /about/i })).toBeInTheDocument();
+  test("should render nav links", () => {
+    render(<Header />, { wrapper });
+
+    const homeLink = screen.getByRole("link", { name: /home/i });
+    const shopLink = screen.getByRole("link", { name: /shop/i });
+    const aboutLink = screen.getByRole("link", { name: /about/i });
+
+    expect(homeLink).toBeInTheDocument();
+    expect(shopLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+
+    expect(homeLink).toHaveAttribute("href", "/");
+    expect(shopLink).toHaveAttribute("href", "/shop");
+    expect(aboutLink).toHaveAttribute("href", "/about");
   });
 });
