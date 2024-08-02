b4ae4504b8494c65368fb8437195d4f2a7af55ca
Bodi_Ali <bodiali59@gmail.com>
2024-08-02 15:49:39 +0300

    Make response show the response status text

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -3,7 +3,7 @@ const API_KEY = "moby_EmWCjGDKP8Zp8WkkBolB5TI6ynj";
 async function getData(params) {
   const response = await fetch(`/api/v1/games${params}api_key=${API_KEY}`);
   if (!response.ok) {
-    throw new Error("Unexpected error occurred");
+    throw new Error(response.statusText);
   }
 
   const data = await response.json();
