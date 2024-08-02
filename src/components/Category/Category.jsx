16c22882ce72d3c3bea6178e98e64f90357dd003
Bodi_Ali <bodiali59@gmail.com>
2024-08-02 15:46:41 +0300

    Create category component to render game cards based on category

---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,38 @@
+import { useLoaderData } from "react-router-dom";
+import getData from "../../utils/fetchData";
+import styles from "./Category.module.css";
+
+function Category() {
+  const games = useLoaderData();
+
+  return (
+    <section className={styles.section}>
+      {games.map((game) => {
+        return (
+          <div key={game.title} className={styles.card}>
+            <div className={styles.imageContainer}>
+              <div
+                className={styles.backGround}
+                style={{ backgroundImage: `url(${game.sample_cover.thumbnail_image})` }}
+              ></div>
+              <img src={game.sample_cover.thumbnail_image} alt={`${game.title} game poster`} />
+            </div>
+            <h3>{game.title}</h3>
+          </div>
+        );
+      })}
+    </section>
+  );
+}
+
+async function loader({ params }) {
+  if (params.category === "games") {
+    const data = await getData("?");
+    return data.games;
+  }
+  return null;
+}
+
+Category.loader = loader;
+
+export default Category;
