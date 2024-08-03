faccdeb3304af1bf3fd68f8e862fb05bf73ff3bb
Bodi_Ali <bodiali59@gmail.com>
2024-08-03 15:24:17 +0300

    Fetch necessary games based on category

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,34 +1,79 @@
-import { useLoaderData } from "react-router-dom";
+import { Await, defer, useLoaderData } from "react-router-dom";
 import getData from "../../utils/fetchData";
 import styles from "./Category.module.css";
+import { Suspense } from "react";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 function Category() {
-  const games = useLoaderData();
+  const { data: games } = useLoaderData();
 
   return (
-    <section className={styles.section}>
-      {games.map((game) => {
-        return (
-          <div key={game.title} className={styles.card}>
-            <div className={styles.imageContainer}>
-              <div
-                className={styles.backGround}
-                style={{ backgroundImage: `url(${game.sample_cover.thumbnail_image})` }}
-              ></div>
-              <img src={game.sample_cover.thumbnail_image} alt={`${game.title} game poster`} />
-            </div>
-            <h3>{game.title}</h3>
-          </div>
-        );
-      })}
-    </section>
+    <Suspense fallback={<LoadingSpinner />}>
+      <Await resolve={games}>
+        {(games) => {
+          return (
+            <section className={styles.section}>
+              {games.games.map((game) => {
+                return (
+                  <div key={game.title} className={styles.card}>
+                    <div className={styles.imageContainer}>
+                      <div
+                        className={styles.backGround}
+                        style={{
+                          backgroundImage: `url(${
+                            game.sample_cover ? game.sample_cover.thumbnail_image : ""
+                          })`,
+                        }}
+                      ></div>
+                      <img
+                        src={game.sample_cover ? game.sample_cover.thumbnail_image : ""}
+                        alt={`${game.title} game poster`}
+                      />
+                    </div>
+                    <h3>{game.title}</h3>
+                  </div>
+                );
+              })}
+              ;
+            </section>
+          );
+        }}
+      </Await>
+    </Suspense>
   );
 }
 
 async function loader({ params }) {
   if (params.category === "games") {
-    const data = await getData("?");
-    return data.games;
+    return defer({ data: getData("?") });
+  } else if (params.category === "windows") {
+    return defer({ data: getData("?platform=3&") });
+  } else if (params.category === "mac") {
+    return defer({ data: getData("?platform=74&") });
+  } else if (params.category === "linux") {
+    return defer({ data: getData("?platform=1&") });
+  } else if (params.category === "playstation") {
+    return defer({ data: getData("?platform=6&") });
+  } else if (params.category === "android") {
+    return defer({ data: getData("?platform=91&") });
+  } else if (params.category === "iphone") {
+    return defer({ data: getData("?platform=86&") });
+  } else if (params.category === "action") {
+    return defer({ data: getData("?genre=1&") });
+  } else if (params.category === "adventure") {
+    return defer({ data: getData("?genre=2&") });
+  } else if (params.category === "education") {
+    return defer({ data: getData("?genre=12&") });
+  } else if (params.category === "puzzle") {
+    return defer({ data: getData("?genre=118&") });
+  } else if (params.category === "arcade") {
+    return defer({ data: getData("?genre=9&") });
+  } else if (params.category === "racing") {
+    return defer({ data: getData("?genre=6&") });
+  } else if (params.category === "simulation") {
+    return defer({ data: getData("?genre=3&") });
+  } else if (params.category === "strategy") {
+    return defer({ data: getData("?genre=4&") });
   }
   throw new Error("Data not found");
 }
