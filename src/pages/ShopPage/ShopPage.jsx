eb7f339a9dd6c5fb7dcf0f2e12269f8a42ea16a1
Bodi_Ali <bodiali59@gmail.com>
2024-08-04 22:35:48 +0300

    Create view cart handler && quantity change handler

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -1,10 +1,49 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { useState } from "react";
+import { NavLink, Outlet, useOutletContext } from "react-router-dom";
+import CartComponent from "../../components/CartComponent/CartComponent";
+import Cart from "../../utils/cart";
 import styles from "./ShopPage.module.css";
 
 function ShopPage() {
+  const [isActive, setIsActive] = useState(false);
+  const [cart, setCart] = useOutletContext();
+
+  function handleViewCart() {
+    setIsActive(!isActive);
+  }
+
+  function handleInputChange(e, game) {
+    const number = parseFloat(e.target.value);
+    if (!isNaN(number) && number > 0) {
+      const quantity = number;
+
+      setCart(
+        new Cart(
+          cart.products.map((product) => {
+            if (product === game) {
+              return {
+                ...product,
+                quantity: quantity,
+                price: product.originalPrice * quantity,
+              };
+            } else {
+              return product;
+            }
+          })
+        )
+      );
+    }
+  }
+
   return (
     <main className={styles.main}>
       <aside className={styles.aside}>
+        <div onClick={handleViewCart} className={styles.viewCart}>
+          <h2>View Cart</h2>
+          <div data-testid="quantity" className={styles.count}>
+            {cart.products.length}
+          </div>
+        </div>
         <h2>Games</h2>
         <ul className={styles.navigation}>
           <NavLink to="games">Games</NavLink>
@@ -43,7 +82,13 @@ function ShopPage() {
           <NavLink to="strategy">Strategy</NavLink>
         </ul>
       </aside>
-      <Outlet />
+      <CartComponent
+        onButtonClick={handleViewCart}
+        cart={cart}
+        handleInputChange={handleInputChange}
+        isActive={isActive}
+      />
+      <Outlet context={[cart, setCart]} />
     </main>
   );
 }
