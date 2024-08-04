2365a5666543ea47c2660e2a60de0c112db4012e
Bodi_Ali <bodiali59@gmail.com>
2024-08-04 22:31:05 +0300

    Pass cart do children

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./components/Header/Header";
 import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
+import Cart from "./utils/cart";
 import "./App.css";
 
 function App() {
+  const [cart, setCart] = useState(new Cart([]));
   const navigation = useNavigation();
   return (
     <>
       <Header />
-      {navigation.state === "loading" ? <LoadingSpinner /> : <Outlet />}
+      {navigation.state === "loading" ? <LoadingSpinner /> : <Outlet context={[cart, setCart]} />}
     </>
   );
 }
