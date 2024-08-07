302b5596d72e009b897fe3420e9a06f3ada12572
Bodi_Ali <bodiali59@gmail.com>
2024-08-08 00:14:46 +0300

    Add total price

--- src/components/CartComponent/CartComponent.jsx
+++ src/components/CartComponent/CartComponent.jsx
@@ -1,42 +1,51 @@
 import PropTypes from "prop-types";
 import Cart from "../../utils/cart";
 import styles from "./CartComponent.module.css";
+
 function CartComponent({ cart, isActive, handleCartVisibility, handleInputChange, handleProductRemove }) {
   let content;
-
   if (cart.products.length === 0) {
     content = <p className={styles.empty}>Cart is empty!</p>;
   } else {
-    content = cart.products.map((product) => {
-      return (
-        <div key={product.title} className={styles.card}>
-          <div className={styles.imageContainer}>
-            <img
-              src={product.sample_cover ? product.sample_cover.thumbnail_image : ""}
-              alt={`${product.title} product poster`}
-            />
-          </div>
-          <div className={styles.textContainer}>
-            <h3>{product.title}</h3>
-            <div className={styles.cartInfoContainer}>
-              <div className={styles.priceContainer}>
-                <p>{`Price: ${product.price}$`}</p>
-                <input
-                  onChange={(e) => handleInputChange(e, product)}
-                  value={product.quantity}
-                  type="number"
-                  name="quantity"
-                  className={styles.quantityInput}
+    content = (
+      <>
+        {cart.products.map((product) => {
+          return (
+            <div key={product.title} className={styles.card}>
+              <div className={styles.imageContainer}>
+                <img
+                  src={product.sample_cover ? product.sample_cover.thumbnail_image : ""}
+                  alt={`${product.title} product poster`}
                 />
               </div>
-              <button onClick={() => handleProductRemove(product)} className={styles.remove}>
-                Remove
-              </button>
+              <div className={styles.textContainer}>
+                <h3>{product.title}</h3>
+                <div className={styles.cartInfoContainer}>
+                  <div className={styles.priceContainer}>
+                    <p>{`Price: ${product.price}$`}</p>
+                    <input
+                      onChange={(e) => handleInputChange(e, product)}
+                      value={product.quantity}
+                      type="number"
+                      name="quantity"
+                      className={styles.quantityInput}
+                    />
+                  </div>
+                  <button onClick={() => handleProductRemove(product)} className={styles.remove}>
+                    Remove
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      );
-    });
+          );
+        })}
+        <button className={styles.checkout}>
+          Checkout
+          <br />
+          <span className={styles.total}>Total: {cart.getAllPrices()}$</span>
+        </button>
+      </>
+    );
   }
 
   return (
@@ -44,10 +53,7 @@ function CartComponent({ cart, isActive, handleCartVisibility, handleInputChange
       <button className={styles.closeButton} onClick={handleCartVisibility}>
         X
       </button>
-      <div className={styles.container}>
-        {content}
-        <button className={styles.checkout}>Checkout</button>
-      </div>
+      <div className={styles.container}>{content}</div>
     </section>
   );
 }
