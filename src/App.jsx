77b24c419deec902195ee489037b4482e6f16be0
Bodi_Ali <bodiali59@gmail.com>
2024-08-07 14:55:32 +0300

    Create function to handle cart adding in App comnponent

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,25 @@ import "./App.css";
 function App() {
   const [cart, setCart] = useState(new Cart([]));
   const navigation = useNavigation();
+  function handleButtonClick(game) {
+    const newCart = new Cart(cart.products);
+    if (newCart.isTheProductPresent(game)) {
+      newCart.removeProduct(game);
+      setCart(newCart);
+    } else {
+      newCart.addProduct(game);
+      setCart(newCart);
+    }
+  }
+
   return (
     <>
       <Header />
-      {navigation.state === "loading" ? <LoadingSpinner /> : <Outlet context={[cart, setCart]} />}
+      {navigation.state === "loading" ? (
+        <LoadingSpinner />
+      ) : (
+        <Outlet context={{ arr: [cart, setCart], handleButtonClick: handleButtonClick }} />
+      )}
     </>
   );
 }
