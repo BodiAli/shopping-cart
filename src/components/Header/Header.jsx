b64bd512d80f7a44ee2aa9c215b79432d2cfd79b
Bodi_Ali <bodiali59@gmail.com>
2024-08-07 17:42:14 +0300

    Feat: Add icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,29 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
 
+// Icons
+import { IoHome } from "react-icons/io5";
+import { IoIosPricetags } from "react-icons/io";
+import { FaCircleExclamation } from "react-icons/fa6";
+
 function Header() {
   return (
     <>
       <header className={styles.header}>
         <h1>GameVault</h1>
         <div className={styles.navContainer}>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="shop/games">Shop</NavLink>
-          <NavLink to="about">About</NavLink>
+          <NavLink to="/">
+            <IoHome />
+            Home
+          </NavLink>
+          <NavLink to="shop/games">
+            <IoIosPricetags />
+            Shop
+          </NavLink>
+          <NavLink to="about">
+            <FaCircleExclamation />
+            About
+          </NavLink>
         </div>
       </header>
     </>
