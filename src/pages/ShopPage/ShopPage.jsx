dc653dd8f07fce1ed10bc3a8a61f25df7d81dbd6
Bodi_Ali <bodiali59@gmail.com>
2024-08-07 17:43:03 +0300

    Feat: Add icons

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -5,10 +5,24 @@ import Cart from "../../utils/cart";
 import styles from "./ShopPage.module.css";
 
 // Icons
-import { SiWindows11 } from "react-icons/si";
+import { SiWindows11, SiApplearcade, SiDwavesystems } from "react-icons/si";
+import { BiSolidCategory } from "react-icons/bi";
 import { CgGames } from "react-icons/cg";
 import { MdLaptopMac } from "react-icons/md";
 import { IoCartOutline } from "react-icons/io5";
+import { VscTerminalLinux } from "react-icons/vsc";
+import { FaPlaystation, FaAppStoreIos } from "react-icons/fa";
+import { TiVendorAndroid } from "react-icons/ti";
+
+import {
+  GiPistolGun,
+  GiMountainRoad,
+  GiOpenBook,
+  GiCarWheel,
+  Gi3dGlasses,
+  GiChessKnight,
+} from "react-icons/gi";
+import { FaPuzzlePiece } from "react-icons/fa6";
 
 function ShopPage() {
   const [isActive, setIsActive] = useState(false);
@@ -16,6 +30,12 @@ function ShopPage() {
   const [cart, setCart] = arr;
   function handleViewCart() {
     setIsActive(!isActive);
+  }
+
+  function handleProductRemove(game) {
+    const newCart = new Cart(cart.products);
+    newCart.removeProduct(game);
+    setCart(newCart);
   }
 
   function handleInputChange(e, game) {
@@ -53,7 +73,10 @@ function ShopPage() {
             {cart.products.length}
           </div>
         </div>
-        <h2>Games</h2>
+        <h2>
+          <CgGames size={"1.3rem"} />
+          Games
+        </h2>
         <ul className={styles.navigation}>
           <NavLink to="games">
             <CgGames size={"1.2rem"} />
@@ -61,10 +84,13 @@ function ShopPage() {
           </NavLink>
         </ul>
 
-        <h2>Platforms</h2>
+        <h2>
+          <SiDwavesystems size={"1.3rem"} />
+          Platforms
+        </h2>
         <ul className={styles.navigation}>
           <NavLink to="windows">
-            <SiWindows11 width={1} />
+            <SiWindows11 />
             Windows
           </NavLink>
 
@@ -73,38 +99,78 @@ function ShopPage() {
             Mac
           </NavLink>
 
-          <NavLink to="linux">Linux</NavLink>
+          <NavLink to="linux">
+            <VscTerminalLinux size={"1.2rem"} />
+            Linux
+          </NavLink>
 
-          <NavLink to="playstation">Playstation</NavLink>
+          <NavLink to="playstation">
+            <FaPlaystation size={"1.2rem"} />
+            Playstation
+          </NavLink>
 
-          <NavLink to="android">Android</NavLink>
+          <NavLink to="android">
+            <TiVendorAndroid size={"1.2rem"} />
+            Android
+          </NavLink>
 
-          <NavLink to="iphone">iPhone</NavLink>
+          <NavLink to="iphone">
+            <FaAppStoreIos size={"1.2rem"} />
+            iPhone
+          </NavLink>
         </ul>
-        <h2>Genres</h2>
+        <h2>
+          <BiSolidCategory size={"1.3rem"} />
+          Genres
+        </h2>
         <ul className={styles.navigation}>
-          <NavLink to="action">Action</NavLink>
+          <NavLink to="action">
+            <GiPistolGun size={"1.2rem"} />
+            Action
+          </NavLink>
 
-          <NavLink to="adventure">Adventure</NavLink>
+          <NavLink to="adventure">
+            <GiMountainRoad size={"1.2rem"} />
+            Adventure
+          </NavLink>
 
-          <NavLink to="education">Educational</NavLink>
+          <NavLink to="education">
+            <GiOpenBook size={"1.2rem"} />
+            Educational
+          </NavLink>
 
-          <NavLink to="puzzle">Puzzle</NavLink>
+          <NavLink to="puzzle">
+            <FaPuzzlePiece size={"1.2rem"} />
+            Puzzle
+          </NavLink>
 
-          <NavLink to="arcade">Arcade</NavLink>
+          <NavLink to="arcade">
+            <SiApplearcade size={"1.2rem"} />
+            Arcade
+          </NavLink>
 
-          <NavLink to="racing">Racing</NavLink>
+          <NavLink to="racing">
+            <GiCarWheel size={"1.2rem"} />
+            Racing
+          </NavLink>
 
-          <NavLink to="simulation">Simulation</NavLink>
+          <NavLink to="simulation">
+            <Gi3dGlasses size={"1.2rem"} />
+            Simulation
+          </NavLink>
 
-          <NavLink to="strategy">Strategy</NavLink>
+          <NavLink to="strategy">
+            <GiChessKnight size={"1.2rem"} />
+            Strategy
+          </NavLink>
         </ul>
       </aside>
       <CartComponent
-        onButtonClick={handleViewCart}
+        handleCartVisibility={handleViewCart}
         cart={cart}
         handleInputChange={handleInputChange}
         isActive={isActive}
+        handleProductRemove={handleProductRemove}
       />
       <Outlet context={{ arr, handleButtonClick }} />
     </main>
