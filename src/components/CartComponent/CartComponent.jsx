abbcc16d86ba917f0e5e7ceded06b41d69d7a3c7
Bodi_Ali <bodiali59@gmail.com>
2024-08-07 17:40:18 +0300

    Refactor component to structure better for conditionally rendering elements

--- src/components/CartComponent/CartComponent.jsx
+++ src/components/CartComponent/CartComponent.jsx
@@ -1,36 +1,52 @@
 import PropTypes from "prop-types";
 import Cart from "../../utils/cart";
 import styles from "./CartComponent.module.css";
-function CartComponent({ cart, isActive, onButtonClick, handleInputChange }) {
-  return (
-    <section className={`${styles.cart} ${isActive ? styles.isActive : ""}`}>
-      <button onClick={onButtonClick}>X</button>
-      <div className={styles.container}>
-        {cart.products.map((product) => {
-          return (
-            <div key={product.title} className={styles.card}>
-              <div className={styles.imageContainer}>
-                <img
-                  src={product.sample_cover ? product.sample_cover.thumbnail_image : ""}
-                  alt={`${product.title} product poster`}
+function CartComponent({ cart, isActive, handleCartVisibility, handleInputChange, handleProductRemove }) {
+  let content;
+
+  if (cart.products.length === 0) {
+    content = <p className={styles.empty}>Cart is empty!</p>;
+  } else {
+    content = cart.products.map((product) => {
+      return (
+        <div key={product.title} className={styles.card}>
+          <div className={styles.imageContainer}>
+            <img
+              src={product.sample_cover ? product.sample_cover.thumbnail_image : ""}
+              alt={`${product.title} product poster`}
+            />
+          </div>
+          <div className={styles.textContainer}>
+            <h3>{product.title}</h3>
+            <div className={styles.cartInfoContainer}>
+              <div className={styles.priceContainer}>
+                <p>{`Price: ${product.price}$`}</p>
+                <input
+                  onChange={(e) => handleInputChange(e, product)}
+                  value={product.quantity}
+                  type="number"
+                  name="quantity"
+                  className={styles.quantityInput}
                 />
               </div>
-              <div className={styles.textContainer}>
-                <h3>{product.title}</h3>
-                <div className={styles.priceContainer}>
-                  <p>{`Price: ${product.price}$`}</p>
-                  <input
-                    onChange={(e) => handleInputChange(e, product)}
-                    value={product.quantity}
-                    type="number"
-                    name="quantity"
-                    className={styles.quantityInput}
-                  />
-                </div>
-              </div>
+              <button onClick={() => handleProductRemove(product)} className={styles.remove}>
+                Remove
+              </button>
             </div>
-          );
-        })}
+          </div>
+        </div>
+      );
+    });
+  }
+
+  return (
+    <section className={`${styles.cart} ${isActive ? styles.isActive : ""}`}>
+      <button className={styles.closeButton} onClick={handleCartVisibility}>
+        X
+      </button>
+      <div className={styles.container}>
+        {content}
+        <button className={styles.checkout}>Checkout</button>
       </div>
     </section>
   );
@@ -39,8 +55,9 @@ function CartComponent({ cart, isActive, onButtonClick, handleInputChange }) {
 CartComponent.propTypes = {
   cart: PropTypes.instanceOf(Cart).isRequired,
   isActive: PropTypes.bool.isRequired,
-  onButtonClick: PropTypes.func.isRequired,
+  handleCartVisibility: PropTypes.func.isRequired,
   handleInputChange: PropTypes.func.isRequired,
+  handleProductRemove: PropTypes.func.isRequired,
 };
 
 export default CartComponent;
