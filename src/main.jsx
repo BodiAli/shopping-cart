3d729055b415ba4e12b8b1afa6eb4687135f07cd
Bodi_Ali <bodiali59@gmail.com>
2024-08-11 20:00:48 +0300

    Feat: Import routes from routes.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,53 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App.jsx";
-import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
-import ErrorComponent from "./components/ErrorComponent/ErrorComponent.jsx";
-import HomePage from "./pages/HomePage/HomePage.jsx";
-import ShopPage from "./pages/ShopPage/ShopPage.jsx";
-import Category from "./components/Category/Category.jsx";
-import ProductPage from "./pages/ProductPage/ProductPage.jsx";
+import routes from "./router/routes.jsx";
 import "./reset.css";
 
 //TODO: Make website responsive
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        errorElement: <ErrorComponent />,
-        children: [
-          {
-            index: true,
-            element: <HomePage />,
-            loader: HomePage.loader,
-          },
-          {
-            path: "shop",
-            element: <ShopPage />,
-            children: [
-              {
-                path: ":category",
-                element: <Category />,
-                loader: Category.loader,
-                errorElement: <ErrorComponent />,
-              },
-            ],
-          },
-          {
-            path: "product/:id",
-            element: <ProductPage />,
-            loader: ProductPage.loader,
-          },
-        ],
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
