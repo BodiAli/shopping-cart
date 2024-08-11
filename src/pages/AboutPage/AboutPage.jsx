935e01442d602c5beac483140adac25d1ae4ae52
Bodi_Ali <bodiali59@gmail.com>
2024-08-11 20:03:01 +0300

    Feat: Create AboutPage component for about route

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,9 +1,44 @@
+import { useEffect } from "react";
 import styles from "./AboutPage.module.css";
 
+// Icons
+import { FaGithub } from "react-icons/fa";
+import { RiGitRepositoryFill } from "react-icons/ri";
+
 function AboutPage() {
+  useEffect(() => {
+    document.title = "About | GameVault";
+
+    return () => {
+      document.title = "GameVault";
+    };
+  }, []);
+
   return (
     <>
-      <h1>HII</h1>
+      <main className={styles.main}>
+        <div className={styles.titleContainer}>
+          <h2 className={styles.title}>GameVault</h2>
+        </div>
+        <p>
+          Welcome to GameVault, your ultimate destination for exploring the world of video games! At
+          GameVault, we bring together a vast collection of games, offering you detailed description,
+          screenshots, and information about your favorite titles.
+        </p>
+      </main>
+      <footer className={styles.footer}>
+        <p>Made by BodiAli</p>
+        <div>
+          <a href="https://github.com/BodiAli">
+            <FaGithub color="#386c5f" />
+          </a>
+          <a href="https://github.com/BodiAli/shopping-cart">
+            <RiGitRepositoryFill color="#386c5f" />
+          </a>
+        </div>
+
+        <p>© {new Date().getFullYear()} GameVault. All rights reserved.</p>
+      </footer>
     </>
   );
 }
