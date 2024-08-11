7ad908f687fa270828311c480a8cd8038aff7fb8
Bodi_Ali <bodiali59@gmail.com>
2024-08-11 20:57:48 +0300

    Fix: Use CORS anywhere to fix netlify deploy

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,8 @@
 const API_KEY = "moby_EmWCjGDKP8Zp8WkkBolB5TI6ynj";
+const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
 
 async function getData(params) {
-  const response = await fetch(`/api/v1/games${params}api_key=${API_KEY}`);
+  const response = await fetch(`${PROXY_URL}https://api.mobygames.com/v1/games${params}api_key=${API_KEY}`);
   if (!response.ok) {
     throw new Error(response.statusText);
   }
