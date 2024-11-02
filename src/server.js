066681c9253b207e5af907b8b0a0695509cf7246
Bodi Ali <bodiali59@gmail.com>
2024-11-03 00:53:51 +0200

    User the port in .env file for deployment

--- src/server.js
+++ src/server.js
@@ -55,7 +55,7 @@ app.get("/api/games", async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
